4293e878d22d9d2a6dd576944f49ea05857c6202
aditya-pm <adityapmenon29@gmail.com>
2026-02-07 23:19:49 +0530

    Add inter particle collision detection

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <time.h>
 
 #include "raylib.h"
@@ -25,6 +26,33 @@ void init_particles() {
     }
 }
 
+void particle_border_collision(Particle* particle) {
+    if (particle->x - particle->radius < 0 || particle->x + particle->radius > WIDTH)
+        particle->velocity_x *= -1;
+
+    if (particle->y - particle->radius < 0 || particle->y + particle->radius > HEIGHT)
+        particle->velocity_y *= -1;
+}
+
+void inter_particle_collision(Particle* particle) {
+    Particle* other_particle;
+    Vector2 particle_center = (Vector2){particle->x, particle->y};
+    Vector2 other_particle_center;
+
+    for (int i = 0; i < PARTICLE_COUNT; i++) {
+        other_particle = &particles[i];
+        if (particle == other_particle) continue;
+
+        other_particle_center = (Vector2){other_particle->x, other_particle->y};
+
+        bool collide = CheckCollisionCircles(particle_center, particle->radius,
+                                             other_particle_center, other_particle->radius);
+
+        if (collide)
+            printf("Collision detected!\n");
+    }
+}
+
 void draw_particle(Particle particle) {
     DrawCircle(particle.x, particle.y, particle.radius, WHITE);
 }
@@ -39,12 +67,8 @@ void update_particle(Particle* particle) {
     particle->x += particle->velocity_x;
     particle->y += particle->velocity_y;
 
-    // collision with walls
-    if (particle->x - particle->radius < 0 || particle->x + particle->radius > WIDTH)
-        particle->velocity_x *= -1;
-
-    if (particle->y - particle->radius < 0 || particle->y + particle->radius > HEIGHT)
-        particle->velocity_y *= -1;
+    particle_border_collision(particle);
+    inter_particle_collision(particle);
 }
 
 void update_particles() {
